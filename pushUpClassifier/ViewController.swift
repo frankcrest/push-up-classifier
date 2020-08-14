b4c0cfbae988f15d013ca31da4f0a97f9e51f580
frankcrest <frankcrestview@hotmail.com>
2020-08-13 23:20:15 -0400

    Initial Commit

---
+++ pushUpClassifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pushUpClassifier
+//
+//  Created by Frank Chen on 2020-08-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue // TODO: Proof we have loaded
+    }
+
+
+}
+
